52c8ebfe2d7c60faca6d53fd370c28b3fcf13911
kyle <kyleghafari@gmail.com>
2020-02-03 16:02:04 -0500

    added notes for build

--- Lab11-ATM_Machine/ATM.cs
+++ Lab11-ATM_Machine/ATM.cs
@@ -6,5 +6,32 @@ namespace Lab11_ATM_Machine
 {
     class ATM
     {
+        //fields
+        private List<Account> userAccounts;
+        //properties
+
+        //constructors
+
+        //methods
+
+        //#1 - Register Account. Takes a username and password and uses them to create a new account, and add to list of active accounts
+
+        //#2 - Login. Takes username and password.
+            // first, check if an account is logged in. If so, kick an error (Current user must log out). 
+            // If not, search the account list for an Account with a matching username AND password 
+            // once found, set that account to the Current Account
+
+
+        // ONLY IF THERE IS AN ACCOUNT LOGGED IN
+        //#3. Logout - Set the current account to null
+
+        //#4. CheckBalance - Prints the current Account Balance (double)
+
+        //#5. Deposit - Takes a double and adds it to the current AccountBalance
+
+        //#6. Withdraw - Takes a double and subtracts from AccountBalance
+            // if AccountBalance < ammountToWithdraw - throw an error.
+
+
     }
 }
--- Lab11-ATM_Machine/Account.cs
+++ Lab11-ATM_Machine/Account.cs
@@ -6,5 +6,16 @@ namespace Lab11_ATM_Machine
 {
     class Account
     {
+        //fields
+        private string name;
+        private string password;
+        private double balance;
+
+        //properties
+
+
+        //constructors
+
+        //methods
     }
 }
