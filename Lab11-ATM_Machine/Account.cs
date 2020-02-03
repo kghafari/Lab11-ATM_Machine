d4bc5c026ddeb026da9d67ea9ad79240ce6d78f1
kyle <kyleghafari@gmail.com>
2020-02-03 15:01:47 -0500

    Just setting up

---
+++ Lab11-ATM_Machine/Account.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11_ATM_Machine
+{
+    class Account
+    {
+    }
+}
---
+++ Lab11-ATM_Machine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab11_ATM_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
